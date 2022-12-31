f832d0efc5175f173620d951f37bccbb59e40a6d
Roshan Thapa <thaparoshan3722@gmail.com>
2022-12-31 14:24:10 +0545

    test

--- class construct/main.c
+++ class construct/main.c
@@ -1,9 +1,27 @@
 #include<stdio.h>
 
+typedef struct
+{
+    int data;
+    int length;
+}cClass;
+
+void cClassConstructor(cClass* o, int d, int l)
+{
+    o->data=d;
+    o->length=l;
+}
+
+int cClassGetLength(cClass* o)
+{
+    return o->length;
+}
+
 int main()
 {
-
-    int temp;
-
+    cClass o1;
+    cClassConstructor(&o1,0,0);
+    int l = cClassGetLength(&o1);
+    printf("%d",l);
     return 0;
 }
